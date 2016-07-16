2ec71dccef52009dda1bb0431c353a25597c7d5b
Gabe Revells <gcrevell@mtu.edu>
2016-07-16 11:52:29 -0400

    Retry

--- BeanCount/StudentsTableViewController.swift
+++ BeanCount/StudentsTableViewController.swift
@@ -156,7 +156,7 @@ class StudentsTableViewController: UITableViewController {
      // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
      }
      }
-     */
+     */ 
     
     /*
      // Override to support rearranging the table view.
