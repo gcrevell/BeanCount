d9f8ac2a7d8a9b83e7dd70c41ae90eb6ed4dd64e
Gabe Revells <gcrevell@mtu.edu>
2016-07-15 20:53:43 -0400

    Initial commit

---
+++ BeanCount/StudentTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  StudentTableViewCell.swift
+//  BeanCount
+//
+//  Created by Voltmeter Amperage on 7/15/16.
+//  Copyright © 2016 Gabriel Revells. All rights reserved.
+//
+
+import UIKit
+
+class StudentTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var checkImageView: UIImageView!
+    var active: Bool = true
+    
+    func update() {
+        if active {
+            checkImageView.image = UIImage(named: "check.png")
+        } else {
+            checkImageView.image = UIImage(named: "uncheck.png")
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
