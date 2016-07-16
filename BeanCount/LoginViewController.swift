110953a9fa0fb2694031f0268e7a5376e4cb62d1
Gabe Revells <gcrevell@mtu.edu>
2016-07-16 15:07:35 -0400

    Created the login page
    Need to set up the ability to create an account and login.

--- BeanCount/LoginViewController.swift
+++ BeanCount/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -14,15 +15,16 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var forgotButton: UIButton!
+    @IBOutlet weak var createButton: UIButton!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subtitleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print("HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
         // Do any additional setup after loading the view.
+        
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard)))
         
         let mainColor = UIColor(red: 150/255, green: 211/255, blue: 41/255, alpha: 1)
         let buttonColor = UIColor(red: 246/255, green: 175/255, blue: 41/255, alpha: 1)
@@ -32,14 +34,16 @@ class LoginViewController: UIViewController {
         
         self.view.backgroundColor = mainColor
         
+        self.usernameField.text = ""
         self.usernameField.backgroundColor = UIColor.white()
         self.usernameField.layer.cornerRadius = 3;
-        self.usernameField.placeholder = "Email Address";
+        self.usernameField.placeholder = "Username";
         self.usernameField.font = UIFont(name: fontName, size: 16)
         
         
         let usernameIcon = UIImageView(frame: CGRect(x: 9, y: 9, width: 24, height: 24))
-        usernameIcon.image = UIImage(named: "mail.png")
+        usernameIcon.image = UIImage(named: "username.png")?.withRenderingMode(.alwaysTemplate)
+        usernameIcon.tintColor = mainColor
         let usernameIconContainer = UIView(frame: CGRect(x: 0, y: 0, width: 41, height: 41))
         usernameIconContainer.backgroundColor = UIColor(white: 0.9, alpha: 1.0)
         usernameIconContainer.addSubview(usernameIcon)
@@ -47,13 +51,16 @@ class LoginViewController: UIViewController {
         self.usernameField.leftViewMode = UITextFieldViewMode.always
         self.usernameField.leftView = usernameIconContainer
         
+        self.passwordField.text = ""
         self.passwordField.backgroundColor = UIColor.white()
         self.passwordField.layer.cornerRadius = 3
         self.passwordField.placeholder = "Password"
         self.passwordField.font = UIFont(name: fontName, size: 16)
         
         let passwordIcon = UIImageView(frame: CGRect(x: 9, y: 9, width: 24, height: 24))
-        passwordIcon.image = UIImage(named: "lock.png")
+        passwordIcon.image = UIImage(named: "lock.png")?.withRenderingMode(.alwaysTemplate)
+        passwordIcon.tintColor = mainColor
+        passwordIcon.contentMode = .scaleAspectFit
         let passwordIconContainer = UIView(frame: CGRect(x: 0, y: 0, width: 41, height: 41))
         passwordIconContainer.backgroundColor = UIColor(white: 0.9, alpha: 1)
         passwordIconContainer.addSubview(passwordIcon)
@@ -71,8 +78,14 @@ class LoginViewController: UIViewController {
         self.forgotButton.backgroundColor = UIColor.clear()
         self.forgotButton.titleLabel?.font = UIFont(name: fontName, size: 12)
         self.forgotButton.setTitle("Forgot password?", for: [])
-        self.forgotButton.setTitleColor(buttonColor, for: [])
+        self.forgotButton.setTitleColor(UIColor.white(), for: [])
         self.forgotButton.setTitleColor(UIColor(white: 1, alpha: 0.5), for: .highlighted)
+        
+        self.createButton.backgroundColor = UIColor.clear()
+        self.createButton.titleLabel?.font = UIFont(name: fontName, size: 12)
+        self.createButton.setTitle("Create an account", for: [])
+        self.createButton.setTitleColor(UIColor.white(), for: [])
+        self.createButton.setTitleColor(UIColor(white: 1, alpha: 0.5), for: .highlighted)
         
         self.titleLabel.textColor = UIColor.white()
         self.titleLabel.font = UIFont(name: boldFont, size: 24)
@@ -81,22 +94,15 @@ class LoginViewController: UIViewController {
         self.subtitleLabel.textColor = UIColor.white()
         self.subtitleLabel.font = UIFont(name: fontName, size: 14)
         self.subtitleLabel.text = "Please login below"
-        
-        
-//        
-//        self.titleLabel.textColor =  [UIColor whiteColor];
-//        self.titleLabel.font =  [UIFont fontWithName:boldFontName size:24.0f];
-//        self.titleLabel.text = @"GOOD TO SEE YOU";
-//        
-//        self.subTitleLabel.textColor =  [UIColor whiteColor];
-//        self.subTitleLabel.font =  [UIFont fontWithName:fontName size:14.0f];
-//        self.subTitleLabel.text = @"Welcome back, please login below";
-//        [self.forgotButton addTarget:self action:@selector(toggleNav:) forControlEvents:UIControlEventTouchUpInside];
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func dismissKeyboard() {
+        self.view.endEditing(false)
     }
     
 
